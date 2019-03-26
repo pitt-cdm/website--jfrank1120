d7122beeee8041a7be17156f14090235a279a4d9
Jared Frank <jfrank1120@verizon.net>
2019-03-26 12:18:37 -0400

    Edited nav bar to look better, added link to personal site when clicking on image on about us page, other minor css color changes

--- docs/js/Script.js
+++ docs/js/Script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     console.log("Script Loaded");
-    $("#info_box").hover(
-        function() {
-            $(this).next().show();
-        },
-        function () {
-            $(this).next().hide();
-        }
-    ); // End Toggle
+    $('#test_id').click(function () {
+        $('#twitter_feed').data('data-height', '1000');
+        console.log("Attempted to change height")
+    });
+    console.log("Run through hover");
+    // Clicking on Image of Jared directs to personal site
+    $('#jared_img').click(function () {
+        window.location.replace("http://jaredfrank.me/");
+    })
 }); // End Ready function
