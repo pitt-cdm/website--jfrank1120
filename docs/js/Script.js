7add1a6a1980c33f44b8007962b80b0ac02cc99f
Jared Frank <jfrank1120@verizon.net>
2019-03-26 23:51:02 -0400

    Created Accordion elements on the information page, added email link to send emails to company email

--- docs/js/Script.js
+++ docs/js/Script.js
@@ -17,4 +17,24 @@ $(document).ready(function () {
         let modal = document.getElementById('jaredModal');
         modal.style.display = 'none';
     });
+
+    // Accordion Code
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
 }); // End Ready function
